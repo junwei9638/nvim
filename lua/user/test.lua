cf22993cb86d4b3640467bf553244d85923d6e02
runruncar0744 <runruncar0744@gmail.com>
2023-11-29 16:11:29 +0800

    color scheme

---
+++ lua/user/test.lua
@@ -0,0 +1,5 @@
+while true do
+   --root folder doesn't get the indentation of the .editorconfig file.
+   --only file within the config directory can.
+   --but hold on a second, let me try an option.
+end
