31a9b9c27619dd4bb8f6d78ef44d54e03a763d52
junwei <junwei9638@cycu.org.tw>
2024-03-13 15:42:42 +0800

    oil

---
+++ lua/user/plugins/oil.lua
@@ -0,0 +1,31 @@
+return {
+   "stevearc/oil.nvim",
+   event = "VeryLazy",
+   dependencies = {
+      "nvim-tree/nvim-web-devicons",
+   },
+   keys = {
+      {
+         "<leader>e",
+         function()
+            require("oil").toggle_float()
+         end,
+         desc = "Oil File Explorer",
+      },
+   },
+   config = function()
+      require("oil").setup({
+         keymaps = {
+            ["h"] = "actions.parent",
+            ["l"] = "actions.select",
+         },
+         view_options = {
+            show_hidden = true,
+         },
+         float = {
+            max_width = 100,
+            max_height = 50,
+         },
+      })
+   end,
+}
